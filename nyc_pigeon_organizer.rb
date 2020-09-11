29cb3e859415753164a566220d392d5a7f0c680c
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-09-11 22:00:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,13 +2,16 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   organized_pigeon = {}
+
   data.each_with_object({}) do |(key, value), organized_pigeon|
     value.each do |new_value, names|
       names.each do |name|
-        binding.pry
+
         if !organized_pigeon[name]
           organized_pigeon[name] = {}
       end
+      if !organized_pigeon[name][key]
+        !organized_pigeon[name][key] = []
     end
   end
     organized_pigeon
