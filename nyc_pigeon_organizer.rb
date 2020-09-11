defcc58f1d78a6c98bb989c4bae1aa44c86c8692
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-09-11 21:59:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized_pigeon = {}
+  data.each_with_object({}) do |(key, value), organized_pigeon|
+    value.each do |new_value, names|
+      names.each do |name|
+        binding.pry
+        if !organized_pigeon[name]
+          organized_pigeon[name] = {}
+      end
+    end
+  end
+    organized_pigeon
+  end
+  binding.pry
 end
